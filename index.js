b93989cb8b677f87ad9e99faf32cece3cf111611
Brad Hambrice <hambrice@uab.edu>
2017-11-20 08:30:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var recipes = {}
 function updateObjectWithKeyAndValue (recipe, key, value) {
   return Object.assign ({}, recipe, {[key]:value});
 }
+function destructivelyUpdateObjectWithKeyAndValue(recipe, key, value) {
+  
+}
