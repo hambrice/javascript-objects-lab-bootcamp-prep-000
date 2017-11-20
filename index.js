a28f98b9c70b2a2faf7a130f59d96c4f91dd26a2
Brad Hambrice <hambrice@uab.edu>
2017-11-20 08:34:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyUpdateObjectWithKeyAndValue(recipe, key, value) {
   recipe[key] = value
   return recipe
 }
-function deleteFromObjectByKey (recipe, key, value) {
+function deleteFromObjectByKey (recipe, key) {
   var newRecipe = Object.assign({}, recipe)
   delete newRecipe.key
   return newRecipe
